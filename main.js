86e5b821e10a5b9c43d62409e60b6a103ab2a5f0
Robert Dawidowski <dawidowski.robert@gmail.com>
2016-05-19 09:11:29 +0200

    slider zajawka

--- main.js
+++ main.js
@@ -18,3 +18,43 @@ $(document).ready(function() {
     });
 
 });
+
+
+$(zajawka).ready(function(){
+    setInterval(function(){
+        $("#zajawka1").fadeOut(6000, function(){
+            $('#zajawka2').fadeOut(6000, function(){
+                    $('#zajawka1').fadeIn(6000);
+            });
+        });
+    }, 3000);
+});
+    //
+    // $(zajawka).ready(function(){
+    // setInterval(function (){
+    //     $("#zajawka1").fadeOut(6000);
+    //     $('#zajawka2').delay(7000).fadeOut(6000);
+    //     // $('#zajawka3').delay(7000).fadeOut(6000);
+    //     $('#zajawka1').delay(7000).fadeIn(6000);
+    //     $('#zajawka2').delay(7000).fadeIn(6000);
+    //
+    // },1500);
+    //
+    // });
+
+//
+// function start() {
+//
+//     $("#zajawka1").fadeOut(6000, function() {
+//         $('#zajawka2').delay(3000.fadeOut(6000);
+//
+//
+//         $('#zajawka1').delay(3000).fadeIn(6000, function() {
+//             $('#zajawka2').delay(3000).fadeIn(6000, start);
+//
+//             setTimeout(start, 6000)
+//         });
+//     })
+// }
+// start();
+
